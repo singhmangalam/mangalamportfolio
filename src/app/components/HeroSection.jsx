1d52e5f189dc7f93e55cf310313abfd0d1c7c96f
singhmangalam <singhmangalam4@gmail.com>
2023-11-10 10:01:53 +0530

    f

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello
             </span>
             <br></br>
             <TypeAnimation
@@ -39,7 +39,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          Frontend Developer specialized in creating engaging and user-friendly web experiences. With a focus on clean, responsive, and visually appealing design, I bring websites to life. Explore my work and let's bring your web projects to the next level.
+          'Frontend Developer specialized in creating engaging and user-friendly web experiences. With a focus on clean, responsive, and visually appealing design, I bring websites to life. Explore my work and let's bring your web projects to the next level.'
           </p>
           <div>
             <Link
