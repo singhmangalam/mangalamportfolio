feca7c563b4128b8cf42117ed32cdddc27293fd9
singhmangalam <singhmangalam4@gmail.com>
2023-11-28 11:02:31 +0530

    f

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -13,19 +13,19 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-<<<<<<< HEAD
+
           Let's Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
       
           Im currently looking for new opportunities, my inbox is always
-=======
+
           Let`s Connect
-        </h5>
+       </p>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
       
           I am currently looking for new opportunities, my inbox is always
->>>>>>> e109a73a2289c72da76c3edda49f31d101ab5906
+
           open. Whether you have a question or just want to say hi, I will
           try my best to get back to you!
         </p>
